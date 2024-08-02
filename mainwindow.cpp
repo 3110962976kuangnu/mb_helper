f49a6fca083b2457def46db7e4a257ecc81a3750
cl <chunrufenglin@outlook.com>
2024-08-02 20:39:00 +0800

    gitignore updated

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,27 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <qcombobox.h>
+#include <qglobal.h>
+#include <qlist.h>
+#include <qserialportinfo.h>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+  get_serial_and_update_ui();
+    // connect(ui->btn_scan_port, SIGNAL(clicked()), this, SLOT(get_serial_and_update_ui()));
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+MainWindow::~MainWindow() { delete ui; }
 
+void MainWindow::get_serial_and_update_ui() {
+  QList<QSerialPortInfo> serialPort_info_list =
+      QSerialPortInfo::availablePorts();
+  ui->cbx_serial_number->clear();
+  qDebug() << "serialPort_info_list size:" << serialPort_info_list.size();
+  for (int i = 0; i < serialPort_info_list.size(); i++) {
+    qDebug() << "serialPort_info_list[" << i
+             << "].portName():" << serialPort_info_list[i].portName();
+    ui->cbx_serial_number->addItem(serialPort_info_list[i].portName());
+  }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,21 +1,28 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QDebug>
 #include <QMainWindow>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
+  void get_serial_and_update_ui();
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow *ui;
+  QSerialPort *serialPort;
 };
 #endif // MAINWINDOW_H
